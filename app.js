773f96eb06637d0ad664460caa5b0991517d9262
Damilare Agba <blankgodd33@gmail.com>
2022-05-16 11:12:20 +0100

    fixing bugs

--- app.js
+++ app.js
@@ -52,16 +52,15 @@ class Monnify {
             'Authorization': `Basic ${key}`
         }
         const data = await this.makeRequest('POST', path, headers)
-        this.accessToken = data.responseBody.accessToken
-        // return data.responseBody.accessToken
+        // this.accessToken = data.responseBody.accessToken
+        return data.responseBody.accessToken
     }
 
     // to reserve an account
-    async reserveAccount(requestBody) {
-        await this.genToken()
+    async reserveAccount(requestBody, accessToken) {
         let path = '/api/v2/bank-transfer/reserved-accounts'
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`,
+            'Authorization': `Bearer ${accessToken}`,
             'Content-Type': "application/json"
         }
         const data = await this.makeRequest('POST', path, headers, requestBody)
@@ -69,22 +68,20 @@ class Monnify {
     }
 
     // to get list of all banks
-    async getBanks() {
-        await this.genToken()
+    async getBanks(accessToken) {
         let path = '/api/v1/banks'
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`
+            'Authorization': `Bearer ${accessToken}`
         }
         const data = await this.makeRequest('GET', path, headers)
         return data
     }
 
     // to make single outbound transfer
-    async singleOutboundTransfer(requestBody) {
-        await this.genToken()
+    async singleOutboundTransfer(requestBody, accessToken) {
         let path = '/api/v2/disbursements/single'
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`,
+            'Authorization': `Bearer ${accessToken}`,
             'Content-Type': "application/json"
         }
         const data = await this.makeRequest('POST', path, headers, requestBody)
@@ -92,11 +89,10 @@ class Monnify {
     }
 
     // to make bulk outbound transfer
-    async bulkOutboundTransfer(requestBody) {
-        await this.genToken()
+    async bulkOutboundTransfer(requestBody, accessToken) {
         let path = '/api/v2/disbursements/batch'
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`,
+            'Authorization': `Bearer ${accessToken}`,
             'Content-Type': "application/json"
         }
         const data = await this.makeRequest('POST', path, headers, requestBody)
@@ -104,11 +100,10 @@ class Monnify {
     }
 
     // to authorize single outbound transfer 
-    async authorizeSingleTransfer(requestBody) {
-        await this.genToken()
+    async authorizeSingleTransfer(requestBody, accessToken) {
         let path = '/api/v2/disbursements/single/validate-otp'
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`,
+            'Authorization': `Bearer ${accessToken}`,
             'Content-Type': "application/json"
         }
         const data = await this.makeRequest('POST', path, headers, requestBody);
@@ -116,11 +111,10 @@ class Monnify {
     }
 
     // to authorize bulk outbound transfer 
-    async authorizeBulkTransfer(requestBody) {
-        await this.genToken()
+    async authorizeBulkTransfer(requestBody, accessToken) {
         let path = '/api/v2/disbursements/batch/validate-otp'
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`,
+            'Authorization': `Bearer ${accessToken}`,
             'Content-Type': "application/json"
         }
         const data = await this.makeRequest('POST', path, headers, requestBody)
@@ -128,22 +122,20 @@ class Monnify {
     }
 
     // resend OTP
-    async resendOtpSingle(requestBody) {
-        await this.genToken()
+    async resendOtpSingle(requestBody, accessToken) {
         let path = '/api/v2/disbursements/single/resend-otp'
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`,
+            'Authorization': `Bearer ${accessToken}`,
             'Content-Type': "application/json"
         }
         const data = await this.makeRequest('POST', path, headers, requestBody)
         return data
     }
 
-    async resendOtpBulk(requestBody) {
-        await this.genToken()
+    async resendOtpBulk(requestBody, accessToken) {
         let path = '/api/v2/disbursements/batch/resend-otp'
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`,
+            'Authorization': `Bearer ${accessToken}`,
             'Content-Type': "application/json"
         }
         const data = await this.makeRequest('POST', path, headers, requestBody)
@@ -151,77 +143,70 @@ class Monnify {
     }
 
     // to get single outbound transfer status
-    async getSingleTransferStatus(reference) {
-        await this.genToken()
+    async getSingleTransferStatus(reference, accessToken) {
         let path = `/api/v2/disbursements/single/summary?reference=${reference}`
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`
+            'Authorization': `Bearer ${accessToken}`
         }
         const data = await this.makeRequest('GET', path, headers)
         return data
     }
 
     // to get bulk outbound transfer status
-    async getBulkTransferStatus(reference) {
-        await this.genToken()
+    async getBulkTransferStatus(reference, accessToken) {
         let path = `/api/v2/disbursements/batch/summary?reference=${reference}`
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`
+            'Authorization': `Bearer ${accessToken}`
         }
         const data = await this.makeRequest('GET', path, headers)
         return data
     }
 
     // to get merchant account wallet balance
-    async getWalletBalance(accountNumber) {
-        await this.genToken()
+    async getWalletBalance(accountNumber, accessToken) {
         let path = `/api/v2/disbursements/wallet-balance?accountNumber=${accountNumber}`
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`
+            'Authorization': `Bearer ${accessToken}`
         }
         const data = await this.makeRequest('GET', path, headers)
         return data
     }
 
     // to get reserved account details
-    async getAccountDetails(reference) {
-        await this.genToken()
+    async getAccountDetails(reference, accessToken) {
         let path = `/api/v2/bank-transfer/reserved-accounts/${reference}`
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`
+            'Authorization': `Bearer ${accessToken}`
         }
         const data = await this.makeRequest('GET', path, headers)
         return data
     }
 
     // for merchant to receive payments
-    async initiatePayment(requestBody) {
-        await this.genToken()
+    async initiatePayment(requestBody, accessToken) {
         let path = '/api/v1/merchant/transactions/init-transaction'
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`,
+            'Authorization': `Bearer ${accessToken}`,
             'Content-Type': "application/json"
         }
         const data = await this.makeRequest('POST', path, headers, requestBody)
         return data
     }
 
-    async payWithBankTransfer(requestBody) {
-        await this.genToken()
+    async payWithBankTransfer(requestBody, accessToken) {
         let path = '/api/v1/merchant/bank-transfer/init-payment'
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`,
+            'Authorization': `Bearer ${accessToken}`,
             'Content-Type': "application/json"
         }
         const data = await this.makeRequest('POST', path, headers, requestBody)
         return data
     }
 
-    async payWithCard(requestBody) {
-        await this.genToken()
+    async payWithCard(requestBody, accessToken) {
         let path = '/api/v1/merchant/cards/charge'
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`,
+            'Authorization': `Bearer ${accessToken}`,
             'Content-Type': "application/json"
         }
         const data = await this.makeRequest('POST', path, headers, requestBody)
@@ -229,76 +214,69 @@ class Monnify {
     }
 
 
-    async authorizeOtpForCard(requestBody) {
-        await this.genToken()
+    async authorizeOtpForCard(requestBody, accessToken) {
         let path = '/api/v1/merchant/cards/otp/authorize'
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`,
+            'Authorization': `Bearer ${accessToken}`,
             'Content-Type': "application/json"
         }
         const data = await this.makeRequest('POST', path, headers, requestBody)
         return data
     }
 
-    async chargeCardToken(requestBody) {
-        await this.genToken()
+    async chargeCardToken(requestBody, accessToken) {
         let path = '/api/v1/merchant/cards/charge-card-token'
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`,
+            'Authorization': `Bearer ${accessToken}`,
             'Content-Type': "application/json"
         }
         const data = await this.makeRequest('POST', path, headers, requestBody)
         return data
     }
 
-    async deleteReservedAccount(accountReference) {
-        await this.genToken()
+    async deleteReservedAccount(accountReference, accessToken) {
         let path = `/api/v1/bank-transfer/reserved-accounts/reference/${accountReference}`
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`
+            'Authorization': `Bearer ${accessToken}`
         }
         const data = await this.makeRequest('DELETE', path, headers)
         return data
     }
 
-    async createLimitProfile(requestBody) {
-        await this.genToken()
+    async createLimitProfile(requestBody, accessToken) {
         let path = '/api/v1/limit-profile/'
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`,
+            'Authorization': `Bearer ${accessToken}`,
             'Content-Type': "application/json"
         }
         const data = await this.makeRequest('POST', path, headers, requestBody)
         return data
     }
 
-    async updateLimitProfile(limitProfileCode, requestBody) {
-        await this.genToken()
+    async updateLimitProfile(limitProfileCode, requestBody, accessToken) {
         let path = `/api/v1/limit-profile/${limitProfileCode}`
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`,
+            'Authorization': `Bearer ${accessToken}`,
             'Content-Type': "application/json"
         }
         const data = await this.makeRequest('PUT', path, headers, requestBody)
         return data
     }
 
-    async getLimitProfiles() {
-        await this.genToken()
+    async getLimitProfiles(accessToken) {
         let path = '/api/v1/limit-profile/'
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`
+            'Authorization': `Bearer ${accessToken}`
         }
         const data = await this.makeRequest('GET', path, headers)
         return data
     }
 
 
-    async reserveAccountWithLimit(requestBody) {
-        await this.genToken()
+    async reserveAccountWithLimit(requestBody, accessToken) {
         let path = '/api/v1/bank-transfer/reserved-accounts/limit'
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`,
+            'Authorization': `Bearer ${accessToken}`,
             'Content-Type': "application/json"
         }
         const data = await this.makeRequest('POST', path, headers, requestBody)
@@ -306,53 +284,48 @@ class Monnify {
     }
 
 
-    async updateReserveAccountLimit(requestBody) {
-        await this.genToken()
+    async updateReserveAccountLimit(requestBody, accessToken) {
         let path = '/api/v1/bank-transfer/reserved-accounts/limit'
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`,
+            'Authorization': `Bearer ${accessToken}`,
             'Content-Type': "application/json"
         }
         const data = await this.makeRequest('PUT', path, headers, requestBody)
         return data
     }
 
-    async initiateRefund(requestBody) {
-        await this.genToken()
+    async initiateRefund(requestBody, accessToken) {
         let path = '/api/v1/refunds/initiate-refund'
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`,
+            'Authorization': `Bearer ${accessToken}`,
             'Content-Type': "application/json"
         }
         const data = await this.makeRequest('POST', path, headers, requestBody)
         return data
     }
 
-    async getAllRefunds(page, size) {
-        await this.genToken()
+    async getAllRefunds(page, size, accessToken) {
         let path = `/api/v1/refunds?page=${page}&size=${size}`
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`
+            'Authorization': `Bearer ${accessToken}`
         }
         const data = await this.makeRequest('GET', path, headers)
         return data
     }
 
-    async getRefundStatus(refundReference) {
-        await this.genToken()
+    async getRefundStatus(refundReference, accessToken) {
         let path = `/api/v1/refunds/${refundReference}`
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`
+            'Authorization': `Bearer ${accessToken}`
         }
         const data = await this.makeRequest('GET', path, headers)
         return data
     }
 
-    async validateBankAccount(accountNumber, bankCode) {
-        await this.genToken()
+    async validateBankAccount(accountNumber, bankCode, accessToken) {
         let path = `/api/v1/disbursements/account/validate?accountNumber=${accountNumber}&bankCode=${bankCode}`
         let headers = {
-            'Authorization': `Bearer ${this.accessToken}`
+            'Authorization': `Bearer ${accessToken}`
         }
         const data = await this.makeRequest('GET', path, headers)
         return data
